776dbc84daf525fe558fa2dbaedd937d5fcc5e8a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-30 10:03:08 -0600

    Add search controller and initial api call

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+  def index
+    api_key = ENV['FOOD_API_KEY']
+    response = Faraday.get(
+      "https://api.nal.usda.gov/fdc/v1/search?api_key=#{ENV['FOOD_API_KEY']}&ingredients=#{params[:search_field]}"
+    )
+
+    @foods = JSON.parse(response.body, symbolize_names: true)
+
+    require 'pry'; binding.pry
+  end
+end
