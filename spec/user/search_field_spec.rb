18a1df872ef871cb50dda3a3cb50b16b6fee8ed4
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-30 09:32:50 -0600

    Add initial spec for searching api

--- spec/user/search_field_spec.rb
+++ spec/user/search_field_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 feature 'USER can search for types of food' do
   scenario 'user submits a valid food type' do
-    
+    visit '/'
+
+    fill_in "search_field",	with: "sweet potatoes"
+
+    click_on 'Search'
+
+    expect(current_path).to eq('/foods')
+    expect(page).to have_content('Items returned: 10')
   end
 end
 
